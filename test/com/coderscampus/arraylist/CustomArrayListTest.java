f1ec643e6d9168842fb51f6b667be1876f8abe06
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2023-09-03 22:32:00 -0500

    added test on Service

---
+++ test/com/coderscampus/arraylist/CustomArrayListTest.java
@@ -0,0 +1,62 @@
+package com.coderscampus.arraylist;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CustomArrayListTest {
+    
+    private CustomArrayList<Integer> customArrayList;
+
+    @BeforeEach
+    public void init() {
+        customArrayList = new CustomArrayList<>();
+    }
+
+    @Test
+    public void testAddMethod() {
+        customArrayList.add(1);
+        customArrayList.add(2);
+        customArrayList.add(3);
+
+        assertEquals(3, customArrayList.getSize());
+    }
+
+    @Test
+    public void testGetSizeMethod() {
+        assertEquals(0, customArrayList.getSize());
+
+        customArrayList.add(1);
+
+        assertEquals(1, customArrayList.getSize());
+    }
+
+    @Test
+    public void testGetMethod() {
+        customArrayList.add(1);
+        customArrayList.add(2);
+        customArrayList.add(3);
+
+        assertEquals(1, customArrayList.get(0));
+        assertEquals(2, customArrayList.get(1));
+        assertEquals(3, customArrayList.get(2));
+    }
+
+    @Test
+    public void testGetMethodThrowsExceptionForInvalidIndex() {
+        assertThrows(IndexOutOfBoundsException.class, () -> customArrayList.get(0));
+        assertThrows(IndexOutOfBoundsException.class, () -> customArrayList.get(-1));
+    }
+
+    @Test
+    public void testArrayResize() {
+        for (int i = 0; i < 11; i++) {
+            customArrayList.add(i);
+        }
+
+        assertEquals(11, customArrayList.getSize());
+        assertEquals(10, customArrayList.get(10));
+    }
+}
