7df3c71d195c58c1ca11614f2e97bd247c7e35aa
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2023-09-06 14:00:27 -0500

    small text changes to main method

--- src/com/coderscampus/arraylist/CustomListApp.java
+++ src/com/coderscampus/arraylist/CustomListApp.java
@@ -5,28 +5,32 @@ public class CustomListApp {
 	public static void main(String[] args) {
 		CustomList<String> myCustomArrayList = new CustomArrayList<String>();
 
-		myCustomArrayList.add("People");
+		myCustomArrayList.add("People:");
 		myCustomArrayList.add("Michael");
-		myCustomArrayList.add("Cat");
+		myCustomArrayList.add("Cat:");
 		myCustomArrayList.add("BumRush");
-		myCustomArrayList.add("Friends");
+		myCustomArrayList.add("Friends:");
 		myCustomArrayList.add("Andrew");
 		myCustomArrayList.add("Brandon");
 		myCustomArrayList.add("Tyler");
 		myCustomArrayList.add("Jed");
-		myCustomArrayList.add("NickPayne");
+		myCustomArrayList.add("Nick Payne");
 		myCustomArrayList.add("Nick Syler");
 		myCustomArrayList.add("Joe");
 		myCustomArrayList.add("Trey");
 		myCustomArrayList.add("Lukas");
 		myCustomArrayList.add("Karen");
+		myCustomArrayList.add("Teacher:");
 		myCustomArrayList.add("Antonio");
-		myCustomArrayList.add("Language");
+		myCustomArrayList.add("Pete");
+		myCustomArrayList.add("Language:");
 		myCustomArrayList.add("C#");
 		myCustomArrayList.add("Java");
 		myCustomArrayList.add("CSS");
 		myCustomArrayList.add("HTML");
 		myCustomArrayList.add("JavaScript");
+		
+		System.out.println("\nOriginal List:");
 
 		for (int i = 0; i < myCustomArrayList.getSize(); i++) {
 			System.out.println(myCustomArrayList.get(i));
